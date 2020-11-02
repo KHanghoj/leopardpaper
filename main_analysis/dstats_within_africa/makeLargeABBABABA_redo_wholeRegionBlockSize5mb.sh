6947002c9e8b8054b790cb2d493ff8bb71082282
KHanghoj <kristianhanghoej@gmail.com>
2020-11-02 16:39:03 +0100

    done

--- main_analysis/dstats_within_africa/makeLargeABBABABA_redo_wholeRegionBlockSize5mb.sh
+++ main_analysis/dstats_within_africa/makeLargeABBABABA_redo_wholeRegionBlockSize5mb.sh
@@ -1,11 +1,4 @@
 angsd=/home/genis/software/angsd/angsd
-
-
-out=/home/casia16/leopard/ABBABABA/results_redo15052020collapsedMapped/largesampleWholeRegionBlockSize5mb_notinkelly
-downsampled=/home/leopard/users/genis/regions/leopDownample_Nregions1000_sizePerRegion1e+05.region
-wholeregion=/home/leopard/users/genis/redo11052020collapsedMapped/infoFiles/autosomes.list
-whitelist=/home/ryan/leopard/ref_regions/mappability_inbreeding_depth_repeat.regions
-bams=/home/leopard/users/genis/redo11052020collapsedMapped/infoFiles/bamsLeopReMapV2No4BadNo8DupNo2Rel.txt
 anc=/home/leopard/users/genis/ref/GCF_001857705.1_PanPar1.0_genomic.s.fna
 
 
